a277023d78c03e827657787865a52114840de012
Jennifer Hajduk <jhajduk@microsoft.com>
2024-06-17 22:04:03 +0000

    scripts

--- run.py
+++ run.py
@@ -1,70 +1,22 @@
-import torch
-import torchvision
-import torchvision.transforms as transforms
-from torch import nn, optim
-import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel
-from torch.utils.data.distributed import DistributedSampler
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+# Load iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
 
-# Define transformations
-transform = transforms.Compose([
-    transforms.Resize(224),  # ResNet requires the input to be 224x224
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalization parameters from ImageNet
-])
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialize the distributed environment
-dist.init_process_group(backend='nccl')
+# Train a logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
 
-# Load the CIFAR10 dataset
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+# Make predictions on the test set and calculate the accuracy
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
 
-# Create distributed samplers
-train_sampler = DistributedSampler(trainset)
-test_sampler = DistributedSampler(testset)
-
-# Create data loaders
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=False, sampler=train_sampler)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, sampler=test_sampler)
-
-# Load the pre-trained ResNet model
-net = torchvision.models.resnet50(pretrained=True)
-
-# Replace the last layer to match the number of classes in CIFAR10
-num_ftrs = net.fc.in_features
-net.fc = nn.Linear(num_ftrs, 10)
-
-# Move the model to GPU and make it distributed
-net = net.to(device)
-net = DistributedDataParallel(net)
-
-# Define the criterion and the optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-# Train the network
-for epoch in range(10):  # loop over the dataset multiple times
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-
-print('Finished Training')
+print(f'Test accuracy: {accuracy}')
